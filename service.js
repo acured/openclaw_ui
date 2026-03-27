967ab418e1e17057608ac98218edb33dedd230d7
Li Na Lu (CSI Interfusion Co Ltd) <v-linalu3@microsoft.com>
2026-03-27 16:30:02 +0800

    first added

---
+++ service.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 5500;
+
+http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './index.html';
+
+    const extname = path.extname(filePath);
+    const contentType = extname === '.html' ? 'text/html' : 'text/javascript';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+}).listen(PORT);
+
+console.log(`服务器已启动：http://localhost:${PORT}`);
